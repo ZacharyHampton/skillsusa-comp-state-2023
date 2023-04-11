b0df0ecec458170848bb442984107c51ff68f2a4
zacharyhampton <zhampton7131@dysartstudents.org>
2023-04-11 11:19:49 -0700

    - proper decryption

--- A/src/encryption/encryption.py
+++ A/src/encryption/encryption.py
@@ -1,14 +1,15 @@
-def _replace_step(number: str) -> str:
+def _replace_step(number: str, is_decrypt: bool = False) -> str:
     """
     Completes first step of encryption, or last step of decryption
 
     :param number: four-digit number
+    :param is_decrypt: Are you decrypting?
     :return: str: number
     """
 
     #: Replace each digit by the (sum of itself + 7) mod 10
     return "".join(
-        [str((int(string_number) + 7) % 10) for string_number in number]
+        [str((int(string_number) + (7 if not is_decrypt else 3)) % 10) for string_number in number]
     )
 
 
@@ -51,5 +52,28 @@ def encrypt(number: str) -> str:
 
     #: Swap second digit with the fourth
     number = _swap(number, index_one=1, index_two=3)
+
+    return number
+
+
+def decrypt(encrypted_number: str) -> str:
+    """
+    Decrypts number using the following algorithm:
+    - Then swap the second digit with the fourth.
+    - Then swap the first digit with the third.
+    - Replace each digit by (the sum of that digit plus 7) modulus 10.
+
+    :param encrypted_number: encrypted number
+    :return: int: four-digit number
+    """
+
+    #: Swap second digit with the fourth
+    encrypted_number = _swap(encrypted_number, index_one=1, index_two=3)
+
+    #: Swap first digit with the third
+    encrypted_number = _swap(encrypted_number, index_one=0, index_two=2)
+
+    #: Replace each digit by the (sum of itself + 7) mod 10
+    number = _replace_step(encrypted_number, is_decrypt=True)
 
     return number
--- A/tests/encrypt.py
+++ A/tests/encrypt.py
@@ -1,7 +1,12 @@
-from encryption.encryption import encrypt
+from encryption.encryption import encrypt, decrypt
 import pytest
 
 
 @pytest.mark.parametrize("number,encrypted_number", [("1234", "0189"), ["9562", "3962"]])
-def test_encryption(number: int, encrypted_number: int):
+def test_encryption(number: str, encrypted_number: str):
     assert encrypt(number) == encrypted_number
+
+
+@pytest.mark.parametrize("encrypted_number,number", [("0189", "1234"), ["3962", "9562"]])
+def test_decryption(encrypted_number: str, number: str):
+    assert decrypt(encrypted_number) == number
